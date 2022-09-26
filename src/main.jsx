c93d8305efb307e185749ea772d744722fc23425
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-26 21:16:44 +0900

    add: recoil

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./store";
+import { RecoilRoot } from "recoil";
 import { TodoPage } from "./pages/todo";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <RecoilRoot>
       <TodoPage />
-    </Provider>
+    </RecoilRoot>
   </React.StrictMode>
 );
